a8f2b8b8d3282e26cbf3d9537d56a9f97b026c60
htrq <mikhail.kiselevk@vk.com>
2026-04-19 12:13:21 +0500

    correct include path

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #define FL_OVERRIDE                                                            \
   override // old fltk versions may put FL_OVERRIDE instead of normal override
 #include "include/mainWindow.hpp"
-#include <Fl/Fl.H>
-#include <Fl/Fl_Double_Window.H>
+#include <FL/Fl.H>
+#include <FL/Fl_Double_Window.H>
 #include <print>
 
 int main() {
