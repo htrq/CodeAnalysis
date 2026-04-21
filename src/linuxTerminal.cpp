e22605c0ac7c808493aba7a03241f93f02bf434d
htrq <mikhail.kiselevk@vk.com>
2026-04-21 20:17:58 +0500

    добавлена обработка клавиш в терминале

--- src/linuxTerminal.cpp
+++ src/linuxTerminal.cpp
@@ -1,34 +1,51 @@
 #include "../include/linuxTerminal.hpp"
-#include <FL/Fl.H>
 #include <FL/Enumerations.H>
+#include <FL/Fl.H>
 #include <FL/Fl_Terminal.H>
 #include <FL/fl_utf8.h>
+#include <asm/termbits.h>
 #include <cstdio>
 #include <cstdlib>
-#include <print>
-#include <sys/types.h>
+#include <cstring>
 #include <fcntl.h>
-#include <unistd.h>
+#include <print>
 #include <sys/ioctl.h>
-#include <asm/termbits.h> 
-
+#include <sys/types.h>
+#include <unistd.h>
 
 void linuxTerminal::enterFirstPromt() { append("echo \"Hellow world\""); }
-
 
 int linuxTerminal::handle(int event) {
   if (event == FL_KEYDOWN) {
     int key = Fl::event_key();
-    const char* text = Fl::event_text();
-      std::println("key {} was pushed", key);
+    const char *text = Fl::event_text();
+    size_t size = strlen(text);
+    std::println("key {} was pushed", key);
+    std::println("Scrollbar size: {}", scrollbar_size());
+
+    switch (key) {
+    case FL_BackSpace:
+      write(master_fd, "\x7f", 1);
+      break;
+    case FL_Enter:
+      write(master_fd, "\n", 1);
+      break;
+    case FL_Up:
+      write(master_fd, "\x1b[A", 3);
+      break;
+    case FL_Down:
+      write(master_fd, "\x1b[B", 3);
+      break;
+    default: 
+      write(master_fd, text, size);
+    }
+    return 1;
   }
 
-  // ВАЖНО: передать событие родителю
   return Fl_Terminal::handle(event);
 }
 
-void linuxTerminal::createPTY()
-{
+void linuxTerminal::createPTY() {
   master_fd = posix_openpt(O_RDWR | O_NOCTTY);
   if (master_fd < 0) {
     perror("posix_openpt");
@@ -36,7 +53,7 @@ void linuxTerminal::createPTY()
   }
   grantpt(master_fd);
   unlockpt(master_fd);
-  char* slave_name = ptsname(master_fd);
+  char *slave_name = ptsname(master_fd);
 
   pid_t pid = fork();
   if (pid == 0) {
@@ -58,13 +75,13 @@ void linuxTerminal::createPTY()
   Fl::add_fd(master_fd, FL_READ, linuxTerminal::pty_cb, this);
 }
 
-void linuxTerminal::pty_cb(int fd, void* userdata) {
-    auto* self = static_cast<linuxTerminal*>(userdata);
-    char buf[4096];
-    ssize_t n = read(fd, buf, sizeof(buf));
-    if (n > 0) {
-        self->append(buf, n);
-    }
+void linuxTerminal::pty_cb(int fd, void *userdata) {
+  auto *self = static_cast<linuxTerminal *>(userdata);
+  char buf[4096];
+  ssize_t n = read(fd, buf, sizeof(buf));
+  if (n > 0) {
+    self->append(buf, n);
+  }
 }
 
-linuxTerminal::~linuxTerminal() = default;
+linuxTerminal::~linuxTerminal() { Fl::remove_fd(master_fd); };
