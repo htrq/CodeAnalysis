fecdb1d1b849177bf7c79ea514a7562b2e9ce981
htrq <mikhail.kiselevk@vk.com>
2026-05-03 13:00:18 +0500

    changed callback mechanism

--- src/tipTree.cpp
+++ src/tipTree.cpp
@@ -1,26 +1,26 @@
 #include "../include/tipTree.hpp"
 #include <FL/Enumerations.H>
-#include <FL/Fl_Box.H>
 #include <FL/Fl.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Group.H>
 #include <FL/Fl_Tooltip.H>
 #include <FL/Fl_Tree.H>
 #include <FL/Fl_Tree_Item.H>
-#include <FL/Fl_Group.H>
 #include <print>
 
 void tipTree::treeCb(Fl_Widget *widget, void *data) {
   auto *tree = static_cast<class tipTree *>(widget);
+  if (tree->callback_reason() != FL_TREE_REASON_SELECTED) {
+    return;
+  }
   tree->itemShowingTip = tree->callback_item();
   if (tree->itemShowingTip != nullptr) {
-    tree->posX = tree->itemShowingTip->x();
-    tree->posY = tree->itemShowingTip->y();
     tree->itemLabel = tree->itemShowingTip->label();
     std::println("Returned item label: {}", tree->itemLabel);
     auto it = tipTree::mapData.find(tree->itemLabel);
     if (it != tipTree::mapData.end()) {
       std::println("Found description: {}", it->second.description);
-      Fl_Tooltip::enter_area(tree, Fl::event_x(), Fl::event_y(), 1, 1,
-                             it->second.description.c_str());
+      Fl_Tooltip::enter_area(tree, 0, 0, 1, 1, it->second.description.c_str());
     }
   }
 }
