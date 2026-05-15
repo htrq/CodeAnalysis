099ed1f6f35d0cfadbb9c14db507f051cd34f7aa
htrq <mikhail.kiselevk@vk.com>
2026-05-15 13:18:26 +0500

    made clear terminal

--- include/linuxTerminal.hpp
+++ include/linuxTerminal.hpp
@@ -2,22 +2,29 @@
 #define LINUX_TERMINAL_HPP_HTRQ
 #include <FL/Fl_Terminal.H>
 
+class linuxTerminal : public Fl_Terminal {
+  Fl_Terminal *flTerminal{};
+  static constexpr int rows{12};
+  static constexpr int cols{89};
+  int master_fd;
 
-
-class linuxTerminal : public Fl_Terminal
-{
-    Fl_Terminal* flTerminal{};
-    static constexpr int rows { 12 };
-    static constexpr int cols { 89 };
-    int master_fd;
 public:
-    using Fl_Terminal::Fl_Terminal;
-    ~linuxTerminal();
-    void enterFirstPromt();
-    void makeTerminalKeyboardEventHandler();
-    int handle(int event) override;
-    void createPTY();
-    static void pty_cb(int fd, void* userdata);
+  linuxTerminal(int X, int Y, int W, int H, const char *label = nullptr)
+      : Fl_Terminal(X, Y, W, H, label) {
+    ansi(true);
+    show_unknown(false);
+
+    display_rows(rows);
+    history_lines(1000);
+
+    scrollbar_size(12);
+  }
+  ~linuxTerminal();
+  void enterFirstPromt();
+  void makeTerminalKeyboardEventHandler();
+  int handle(int event) override;
+  void createPTY();
+  static void pty_cb(int fd, void *userdata);
 };
 
 #endif
--- src/linuxTerminal.cpp
+++ src/linuxTerminal.cpp
@@ -12,8 +12,9 @@
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <regex>
 
-void linuxTerminal::enterFirstPromt() { append("echo \"Hellow world\""); }
+void linuxTerminal::enterFirstPromt() { write(master_fd, "Hellow world\n", 16); }
 
 int linuxTerminal::handle(int event) {
   if (event == FL_KEYDOWN) {
@@ -46,42 +47,95 @@ int linuxTerminal::handle(int event) {
 }
 
 void linuxTerminal::createPTY() {
-  master_fd = posix_openpt(O_RDWR | O_NOCTTY);
-  if (master_fd < 0) {
-    perror("posix_openpt");
-    exit(1);
-  }
-  grantpt(master_fd);
-  unlockpt(master_fd);
-  char *slave_name = ptsname(master_fd);
+    master_fd = posix_openpt(O_RDWR | O_NOCTTY);
+    if (master_fd < 0) {
+        perror("posix_openpt");
+        exit(1);
+    }
 
-  pid_t pid = fork();
-  if (pid == 0) {
-    setsid();
-    int slave_fd = open(slave_name, O_RDWR);
-    ioctl(slave_fd, TIOCSCTTY, 0);
+    grantpt(master_fd);
+    unlockpt(master_fd);
 
-    dup2(slave_fd, STDIN_FILENO);
-    dup2(slave_fd, STDOUT_FILENO);
-    dup2(slave_fd, STDERR_FILENO);
+    char *slave_name = ptsname(master_fd);
 
-    close(master_fd);
-    close(slave_fd);
+    pid_t pid = fork();
 
-    execlp("bash", "bash", nullptr);
-    perror("execlp");
-    exit(1);
-  }
-  Fl::add_fd(master_fd, FL_READ, linuxTerminal::pty_cb, this);
+    if (pid == 0) {
+        setsid();
+
+        int slave_fd = open(slave_name, O_RDWR);
+
+        ioctl(slave_fd, TIOCSCTTY, 0);
+
+        winsize ws{};
+        ws.ws_row = 24;
+        ws.ws_col = 80;
+        ioctl(slave_fd, TIOCSWINSZ, &ws);
+
+        dup2(slave_fd, STDIN_FILENO);
+        dup2(slave_fd, STDOUT_FILENO);
+        dup2(slave_fd, STDERR_FILENO);
+
+        close(master_fd);
+        close(slave_fd);
+
+        setenv("TERM", "xterm-256color", 1);
+
+        execlp("bash", "bash", nullptr);
+
+        perror("execlp");
+        exit(1);
+    }
+
+    Fl::add_fd(master_fd, FL_READ,
+               linuxTerminal::pty_cb, this);
 }
 
-void linuxTerminal::pty_cb(int fd, void *userdata) {
-  auto *self = static_cast<linuxTerminal *>(userdata);
-  char buf[4096];
-  ssize_t n = read(fd, buf, sizeof(buf));
-  if (n > 0) {
-    self->append(buf, n);
-  }
+void linuxTerminal::pty_cb(int fd, void *userdata)
+{
+    auto *self = static_cast<linuxTerminal *>(userdata);
+
+    char buf[4096];
+    ssize_t n = read(fd, buf, sizeof(buf));
+
+    if (n <= 0)
+        return;
+
+    std::string out;
+    out.reserve(n);
+
+    for (ssize_t i = 0; i < n; ++i)
+    {
+        // ESC
+        if (buf[i] == '\x1b')
+        {
+            // CSI: ESC[?2004h/l
+            if (i + 1 < n && buf[i + 1] == '[')
+            {
+                while (i < n &&
+                       !std::isalpha(buf[i]))
+                {
+                    ++i;
+                }
+                continue;
+            }
+
+            // OSC: ESC ] ... BEL
+            if (i + 1 < n && buf[i + 1] == ']')
+            {
+                i += 2;
+
+                while (i < n && buf[i] != '\a')
+                    ++i;
+
+                continue;
+            }
+        }
+
+        out += buf[i];
+    }
+
+    self->append(out.c_str());
 }
 
 linuxTerminal::~linuxTerminal() { Fl::remove_fd(master_fd); };
